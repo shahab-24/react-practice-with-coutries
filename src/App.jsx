781a3328ed78ebdb25fe58b4d26e1f8395469179
SHAHAB UDDIN <shahab.82946@gmail.com>
2024-10-22 19:21:34 +0600

    countries data stored with useEffect and set with useState and displayed name,flags

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
+import Countries from './components/Countries/Countries'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       
       <h1>Vite + React</h1>
+      <Countries></Countries>
      
       
     </>
---
+++ src/components/Countries/Country.jsx
@@ -0,0 +1,15 @@
+
+
+const Country = ({country}) => {
+    const {name,flags} = country;
+  
+    return (
+        <div className="country">
+            
+            <h2>Name: {name.common}</h2>
+            <img src={flags.png} alt="" />
+        </div>
+    );
+};
+
+export default Country;
