ba2d95d9353720e2457584fd1245d4487d315cb3
SHAHAB UDDIN <shahab.82946@gmail.com>
2024-10-22 21:24:22 +0600

    add event handler to button implement condition toggle with state

--- src/components/Countries/Country.jsx
+++ src/components/Countries/Country.jsx
@@ -1,13 +1,24 @@
-
+import { useState } from 'react';
+import './Country.css'
 
 const Country = ({country}) => {
-    const {name,flags} = country;
+    const {name,flags, capital, area, cca3} = country;
   
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited)
+    }
     return (
         <div className="country">
             
             <h2>Name: {name.common}</h2>
             <img src={flags.png} alt="" />
+            <h4>Capital: {capital}</h4>
+            <p>Area: {area}</p>
+            <p><small>cca: {cca3}</small></p>
+            <button className='btn btn-ghost' onClick={handleVisited}>{visited ? 'Visited' : "Not Visited"}</button>
+            {/* {visited ? 'i have visited' : "not visited yet"} */}
         </div>
     );
 };
